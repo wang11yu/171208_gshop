585923ef39a0911bb8b6218a6f47b49db212e8ab
wang11yu <2306698561@qq.com>
2020-12-03 17:14:15 +0800

    init gshop

---
+++ src/api/ajax.js
@@ -0,0 +1,45 @@
+/*
+* ajax 请求函数模块
+* 返回值：promise对象（response.data）
+* */
+
+import axios from 'axios'
+export default function ajax(url,data={},type='Get'){
+ return  new promise(function(resolve,reject){
+//异步请求
+   let promise
+   if (type === 'GET') {
+// 准备 url query 参数数据
+     let dataStr = '' //数据拼接字符串
+     Object.keys(data).forEach(key => {
+       dataStr += key + '=' + data[key] + '&'
+     })
+     if (dataStr !== '') {
+       dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+       url = url + '?' + dataStr
+     }
+// 发送 get 请求
+     promise = axios.get(url)
+   } else {
+// 发送 post 请求
+     promise = axios.post(url, data)
+   }
+
+  })
+
+    promise.then(function(response){
+        resolve(response.data)
+    })
+      .catch(function(error){
+              reject(error)
+
+      })
+
+}
+/*
+* const response=await ajax()
+const result=response.data
+
+const result=await.ajax()
+* */
+
